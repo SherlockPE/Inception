8f61bb5b3c96ee1e881335f8fe5f69c92121714c
SherlockPE <fabriciolopezreyes@gmail.com>
2025-01-28 20:35:31 +0100

    setup of mariadb done

---
+++ srcs/requirements/mariadb/conf/database-setup.sql
@@ -0,0 +1,7 @@
+
+
+Hi, my name is ${LOGIN} and my domain is {DOMAIN}
+
+The mariadb User is ${MDB_USER} and the password is ${MDB_USER_PASSWORD}
+
+The admin password is ${MDB_ADMIN_PASSWORD}
--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+
+
+
+
 # apk add mariadb
 
 
