5d9fcae9a7c32954f44d8fb7feb712869d7b2c95
SherlockPE <fabriciolopezreyes@gmail.com>
2025-01-29 19:47:27 +0100

    refactor: update Nginx Dockerfile and rename configuration script for improved clarity and functionality

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,13 +1,17 @@
 #!bin/sh
 
-# Hello there!
-# General Kenobi
+export LOGIN=flopez-r
 
 # Crear directorio para SSL
 mkdir -p etc/nginx/ssl
+
+# Reemplazar variables de entorno
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bk
+envsubst < /etc/nginx/nginx.conf.bk > /etc/nginx/nginx.conf
 
 # Generar clave privada y certificado autofirmado
 openssl req -x509 -nodes -days 42 -newkey rsa:4096 -keyout /etc/nginx/ssl/${LOGIN}.key -out /etc/nginx/ssl/${LOGIN}.crt -subj "/C=ES/ST=Madrid/L=Madrid/O=42Madrid/CN=flopez-r.42.fr"
 
 # Iniciar nginx
-nginx -g 'daemon off;'
+# nginx -g 'daemon off;'
+sleep infinity & wait
